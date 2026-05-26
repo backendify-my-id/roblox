03fd38bb7b24d3aa9558ed65b3a0b689e607f6df
Johanes Nicky C. S <apanyaclay1@gmail.com>
2026-05-26 23:14:46 +0700

    Update index.html precache revision
    Bump the Workbox precache revision for index.html in frontend/dev-dist/sw.js to force a cache refresh. Replaced revision "0.i1qhg95ce6g" with "0.0a6os965hu8" in the service worker manifest.

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-5d155c7a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.i1qhg95ce6g"
+    "revision": "0.0a6os965hu8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
